7436c8693646bfe73c9155701eca83ef7ef6fc53
Dane Bishop <115124397+danebishop@users.noreply.github.com>
2025-02-26 15:54:40 +0000

    Domain Model Integrated

--- BusShuttleProject/BusShuttle.Tests/FileSaverTests.cs
+++ BusShuttleProject/BusShuttle.Tests/FileSaverTests.cs
@@ -21,4 +21,18 @@ public class fileSaverTests
         Assert.Equal("Hello, World"+Environment.NewLine, contentFromFile);
 
     }
+
+    [Fact]
+    public void Test_FileSaver_AppendData()
+    {   
+        Stop sampleStop = new Stop("SampleStop");
+        Loop sampleLoop = new Loop("SampleLoop");
+        Driver sampleDriver = new Driver("SampleDriver");
+        PassengerData sampleData = new PassengerData(5, sampleStop, sampleLoop, sampleDriver);
+
+        fileSaver.AppendData(sampleData);
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal("SampleDriver:SampleLoop:SampleStop:5"+Environment.NewLine, contentFromFile);
+
+    }
 }
